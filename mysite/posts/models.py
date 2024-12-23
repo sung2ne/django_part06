e3449bd21e93382045b7baf68fad74711684a5ed
정필성 <ibetter.kr@gmail.com>
2024-12-23 23:05:20 +0900

    코드 수정

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
+# 게시글 모델
 class Posts(models.Model):
-    # 게시글 정보
     title = models.CharField(verbose_name="제목", max_length=100)
     content = models.TextField(verbose_name="내용")
     password = models.CharField(verbose_name="비밀번호", max_length=100)
